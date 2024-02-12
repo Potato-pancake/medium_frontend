c71bf43028c06e0ead1a7d236a3da1b7fb697191
sebanimm <gks32114221@gmail.com>
2024-02-12 12:32:29 +0900

    feat: 작업물 컴포넌트

---
+++ components/Artwork/index.tsx
@@ -0,0 +1,20 @@
+interface ArtworkPropsType {
+  artworkName: string;
+  artistName: string;
+}
+
+const Artwork = ({ artworkName, artistName }: ArtworkPropsType) => {
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="w-[304px] h-[200px] bg-slate-500" />
+      <div className="flex flex-col gap-0.5">
+        <span className="text-xl text-black font-semibold">{artworkName}</span>
+        <span className="text-base text-gray-400 font-normal">
+          {artistName}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Artwork;
