842e2182d0cf46aa0b4231d144440fe2a68ce03f
sebanimm <gks32114221@gmail.com>
2024-02-15 19:56:37 +0900

    feat: 시멘틱 태그 적용

--- components/Artwork/index.tsx
+++ components/Artwork/index.tsx
@@ -8,10 +8,8 @@ const Artwork = ({ artworkName, artistName }: ArtworkPropsType) => {
     <div className="flex flex-col gap-3">
       <div className="w-full h-[170px] bg-slate-500" />
       <div className="flex flex-col gap-0.5">
-        <span className="text-xl text-black font-semibold">{artworkName}</span>
-        <span className="text-base text-gray-400 font-normal">
-          {artistName}
-        </span>
+        <p className="text-xl text-black font-semibold">{artworkName}</p>
+        <p className="text-base text-gray-400 font-normal">{artistName}</p>
       </div>
     </div>
   );
--- pages/board/layout/index.tsx
+++ pages/board/layout/index.tsx
@@ -3,10 +3,10 @@ import Artwork from "@/components/Artwork";
 const BoardLayout = () => {
   return (
     <div className="min-h-screen my-[156px]">
-      <div className="max-w-[1120px] w-[calc(100%-2rem)] flex flex-col gap-8 m-auto">
-        <span className="text-5xl text-black font-bold">게시판</span>
+      <main className="max-w-[1120px] w-[calc(100%-2rem)] flex flex-col gap-8 m-auto">
+        <h1 className="text-5xl text-black font-bold">게시판</h1>
         <div className="flex flex-col items-center gap-12">
-          <div className="w-full grid grid-cols-4 gap-x-5 gap-y-12">
+          <article className="w-full grid grid-cols-4 gap-x-5 gap-y-12">
             {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((value) => (
               <Artwork
                 key={value}
@@ -14,12 +14,12 @@ const BoardLayout = () => {
                 artistName="아티스트"
               />
             ))}
-          </div>
+          </article>
           <button className="w-[98px] h-[44px] text-lg text-white font-normal bg-gray-900">
             더보기
           </button>
         </div>
-      </div>
+      </main>
     </div>
   );
 };
