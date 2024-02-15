9b96eafafab0c21800688159553c42eeee9f5878
sebanimm <gks32114221@gmail.com>
2024-02-15 15:19:36 +0900

    fix: 게시판 수정

--- components/Artwork/index.tsx
+++ components/Artwork/index.tsx
@@ -6,7 +6,7 @@ interface ArtworkPropsType {
 const Artwork = ({ artworkName, artistName }: ArtworkPropsType) => {
   return (
     <div className="flex flex-col gap-3">
-      <div className="w-[304px] h-[200px] bg-slate-500" />
+      <div className="w-full h-[170px] bg-slate-500" />
       <div className="flex flex-col gap-0.5">
         <span className="text-xl text-black font-semibold">{artworkName}</span>
         <span className="text-base text-gray-400 font-normal">
--- pages/board/layout/index.tsx
+++ pages/board/layout/index.tsx
@@ -3,11 +3,11 @@ import Artwork from "@/components/Artwork";
 const BoardLayout = () => {
   return (
     <div className="min-h-screen my-[156px]">
-      <div className="max-w-[1600px] flex flex-col gap-8 mx-40">
+      <div className="max-w-[1120px] w-[calc(100%-2rem)] flex flex-col gap-8 m-auto">
         <span className="text-5xl text-black font-bold">게시판</span>
         <div className="flex flex-col items-center gap-12">
-          <div className="flex gap-5 flex-wrap">
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+          <div className="w-full grid grid-cols-4 gap-x-5 gap-y-12">
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((value) => (
               <Artwork
                 key={value}
                 artworkName="작업물 제목"
