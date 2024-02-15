bed357622fef76510f7b630d325bb96b88f6ca8b
sebanimm <gks32114221@gmail.com>
2024-02-15 20:00:17 +0900

    feat: 시멘틱태그 적용

--- components/Artwork/index.tsx
+++ components/Artwork/index.tsx
@@ -5,13 +5,13 @@ interface ArtworkPropsType {
 
 const Artwork = ({ artworkName, artistName }: ArtworkPropsType) => {
   return (
-    <div className="flex flex-col gap-3">
+    <article className="flex flex-col gap-3">
       <div className="w-full h-[170px] bg-slate-500" />
       <div className="flex flex-col gap-0.5">
         <p className="text-xl text-black font-semibold">{artworkName}</p>
         <p className="text-base text-gray-400 font-normal">{artistName}</p>
       </div>
-    </div>
+    </article>
   );
 };
 
--- pages/board/layout/index.tsx
+++ pages/board/layout/index.tsx
@@ -6,7 +6,7 @@ const BoardLayout = () => {
       <main className="max-w-[1120px] w-[calc(100%-2rem)] flex flex-col gap-8 m-auto">
         <h1 className="text-5xl text-black font-bold">게시판</h1>
         <div className="flex flex-col items-center gap-12">
-          <article className="w-full grid grid-cols-4 gap-x-5 gap-y-12">
+          <section className="w-full grid grid-cols-4 gap-x-5 gap-y-12">
             {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((value) => (
               <Artwork
                 key={value}
@@ -14,7 +14,7 @@ const BoardLayout = () => {
                 artistName="아티스트"
               />
             ))}
-          </article>
+          </section>
           <button className="w-[98px] h-[44px] text-lg text-white font-normal bg-gray-900">
             더보기
           </button>
